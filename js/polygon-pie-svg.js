87e7a0f8ac49be394324028b13edf6b95983bb4b
Anthony Cossins <anthonyecossins@gmail.com>
2013-07-01 17:47:30 +0100

    Got the basic SVG pie chart stuff working. This is a good time for everyone

--- js/polygon-pie-svg.js
+++ js/polygon-pie-svg.js
@@ -15,57 +15,10 @@ var PolygonPie = function ( options ) {
 		var optionObject = options;
 	} else {
 		var optionObject = defaultOptions;
-	}
-
-	function trigFunctionX ( pointRatio ){
-		return Math.cos(pointRatio*2*Math.PI);
-	}
-
-	function trigFunctionY ( pointRatio ){
-		return Math.sin(pointRatio*2*Math.PI);
-	}                                        
+	}                                 
 
 	this.draw = function ( context ) {
-		this.centerX = optionObject.x;
-		this.centerY = optionObject.y;
-		this.radius = optionObject.radius;
-		this.sides = optionObject.sides;
-		this.percents = optionObject.percents;
-
-		/* Triangle drawing */
-		for (var i=0; i<=this.sides; i++) {
-
-			console.log('YE');
-
-			$('.pie-debug').append('<polygon points="0 0, 100 0, 50 100" transform="rotate(0 0 0)"/>');
-			$('.pie-chart').append('<p>Test</p>');
-
-			// var triRadius = (this.radius*(this.percents[i]/100));
-			// var pointCount = i;
-			// var pointRatio = pointCount/this.sides;
-			// var pointRatioPRev = (pointCount+1)/this.sides;
-
-			// var xSteps = trigFunctionX(pointRatio);
-			// var ySteps = trigFunctionY(pointRatio);
-
-			// var xStepsPrev = trigFunctionX(pointRatioPRev);
-			// var yStepsPrev = trigFunctionY(pointRatioPRev);
-
-			// var pointX = this.centerX + xSteps * triRadius;
-			// var pointY = this.centerY + ySteps * triRadius;
-
-			// var pointXPrev = this.centerX + xStepsPrev * triRadius;
-			// var pointYPrev = this.centerY + yStepsPrev * triRadius;
-
-			// context.beginPath();
-			// context.moveTo(pointX,  pointY);
-			// context.lineTo(this.centerX, this.centerY);
-			// context.lineTo(pointXPrev, pointYPrev);
-			// context.lineTo(pointX, pointY);
-			// context.closePath();
-			// context.fillStyle = this.segmentColors[i];
-			// context.fill();
-		}
+		
 	}
 }
 
